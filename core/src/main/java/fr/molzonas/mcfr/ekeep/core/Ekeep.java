e51982898c4af65fc771c890b7ecfeb41fc0d357
Molzonas <molzonas@outlook.fr>
2025-09-21 05:38:19 +0200

    Adding more API + GitHub CI/CD + Qodana + config.yml

--- core/src/main/java/fr/molzonas/mcfr/ekeep/core/Ekeep.java
+++ core/src/main/java/fr/molzonas/mcfr/ekeep/core/Ekeep.java
@@ -1,7 +1,6 @@
 package fr.molzonas.mcfr.ekeep.core;
 
 import fr.molzonas.mcfr.ekeep.core.utils.GitUpdateChecker;
-import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Ekeep extends JavaPlugin {
@@ -9,7 +8,6 @@ public final class Ekeep extends JavaPlugin {
     @Override
     public void onEnable() {
         // TODO everything
-
         GitUpdateChecker.logCheckAsync(this);
     }
 
