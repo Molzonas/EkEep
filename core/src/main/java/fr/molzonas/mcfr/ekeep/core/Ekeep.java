d1f753ec4a70478389b178bbcec59c1c30413097
Molzonas <molzonas@outlook.fr>
2025-09-24 03:46:18 +0200

    Adding config initialization in main class

--- core/src/main/java/fr/molzonas/mcfr/ekeep/core/Ekeep.java
+++ core/src/main/java/fr/molzonas/mcfr/ekeep/core/Ekeep.java
@@ -1,18 +1,38 @@
 package fr.molzonas.mcfr.ekeep.core;
 
+import fr.molzonas.mcfr.ekeep.core.configs.Config;
 import fr.molzonas.mcfr.ekeep.core.utils.GitUpdateChecker;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Ekeep extends JavaPlugin {
+    private static Ekeep instance;
+    private Config mainConfig;
 
     @Override
     public void onEnable() {
+        instance = this;
+
+        initConfig();
+
         // TODO everything
+
         GitUpdateChecker.logCheckAsync(this);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    private void initConfig() {
+        this.mainConfig = new Config(this.getConfig());
+    }
+
+    public Config getMainConfig() {
+        return this.mainConfig;
+    }
+
+    public static Ekeep getInstance() {
+        return instance;
     }
 }
